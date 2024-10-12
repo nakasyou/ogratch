5e357bb7967768bd74d355d5893049f3b417cb72
-akku- <151517253+akku1139@users.noreply.github.com>
2024-10-12 17:52:23 +0900

    projects/ route

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { Hono } from 'hono'
+import { Hono, type Handler } from 'hono'
 import { html } from 'hono/html'
 
 const app =
@@ -73,184 +73,192 @@ app
       )
     },
   )
+
+const projRoute: Handler = async (
+  c,
+) => {
+  const projectId =
+    c.req
+      .param(
+        'projectId',
+      )
+  const link =
+    `https://scratch.mit.edu/projects/${projectId}`
+
+  const userAgent =
+    (c.req
+      .header(
+        'User-Agent',
+      ) ||
+      '')
+      .toLowerCase()
+  console
+    .log(
+      userAgent,
+    )
+  if (
+    !(
+      userAgent
+        .includes(
+          'twitter',
+        ) ||
+      userAgent
+        .includes(
+          'discord',
+        ) ||
+      userAgent
+        .includes(
+          'facebook',
+        ) ||
+      userAgent
+        .includes(
+          'slackbot',
+        )
+    )
+  ) {
+    console
+      .log(
+        'nonbot',
+      )
+    return c
+      .redirect(
+        link,
+        302,
+      )
+  }
+  let data =
+    await fetch(
+      `https://trampoline.turbowarp.org/api/projects/${projectId}`,
+    ).then(
+      (
+        res,
+      ) =>
+        res
+          .json(),
+    )
+
+  if (
+    data
+      .error
+  ) {
+    data =
+      {
+        title:
+          'unknown',
+        description:
+          'unknown',
+      }
+  }
+  const desc =
+    data
+      .description
+  const embed =
+    {
+      author_name:
+        `👁️${data.stats.views} ❤️${data.stats.loves} ⭐${data.stats.favorites} 🌀${data.stats.remixes} | ${data.author.username}`,
+      author_url:
+        `https://scratch.mit.edu/users/${data.author.username}`,
+      author_icon:
+        data
+          .author
+          .profile
+          .images[
+            '90x90'
+          ],
+      provider_name:
+        'ogratch',
+      provider_url:
+        'https://github.com/nakasyou/ogratch',
+      title:
+        'Scratch',
+      'type':
+        'link',
+      'version':
+        '1.0',
+    }
+  return c
+    .html(
+      html`<!DOCTYPE HTML>${(
+        <html lang='ja'>
+          <head prefix='og: http://ogp.me/ns#'>
+            <meta charset='UTF-8' />
+            <title>
+              Ogratch
+            </title>
+            <meta
+              name='viewport'
+              content='width=device-width, initial-scale=1'
+            />
+            <meta
+              name='description'
+              content=''
+            />
+
+            <meta
+              property='og:site_name'
+              content='Scratch'
+            />
+            <meta
+              property='og:title'
+              content={data
+                .title}
+            />
+            <meta
+              property='og:type'
+              content='article'
+            />
+            <meta
+              property='og:url'
+              content={link}
+            />
+            <meta
+              property='og:image'
+              content={`https://ogratch.deno.dev/thumbnail/p/${projectId}`}
+            />
+            <meta
+              property='og:description'
+              content={desc}
+            />
+            <meta
+              name='twitter:card'
+              content='summary_large_image'
+            />
+            <link
+              rel='alternate'
+              href={`https://ogratch.deno.dev/echo-json/${
+                encodeURIComponent(
+                  JSON
+                    .stringify(
+                      embed,
+                    ),
+                )
+              }`}
+              type='application/json+oembed'
+              title='Scratch'
+            />
+          </head>
+          <body>
+            <a
+              href={link}
+            >
+              Push!
+            </a>
+          </body>
+        </html>
+      )}`,
+    )
+}
 app
   .get(
     '/p/:projectId',
-    async (
-      c,
-    ) => {
-      const projectId =
-        c.req
-          .param(
-            'projectId',
-          )
-      const link =
-        `https://scratch.mit.edu/projects/${projectId}`
-
-      const userAgent =
-        (c.req
-          .header(
-            'User-Agent',
-          ) ||
-          '')
-          .toLowerCase()
-      console
-        .log(
-          userAgent,
-        )
-      if (
-        !(
-          userAgent
-            .includes(
-              'twitter',
-            ) ||
-          userAgent
-            .includes(
-              'discord',
-            ) ||
-          userAgent
-            .includes(
-              'facebook',
-            ) ||
-          userAgent
-            .includes(
-              'slackbot',
-            )
-        )
-      ) {
-        console
-          .log(
-            'nonbot',
-          )
-        return c
-          .redirect(
-            link,
-            302,
-          )
-      }
-      let data =
-        await fetch(
-          `https://trampoline.turbowarp.org/api/projects/${projectId}`,
-        ).then(
-          (
-            res,
-          ) =>
-            res
-              .json(),
-        )
-
-      if (
-        data
-          .error
-      ) {
-        data =
-          {
-            title:
-              'unknown',
-            description:
-              'unknown',
-          }
-      }
-      const desc =
-        data
-          .description
-      const embed =
-        {
-          author_name:
-            `👁️${data.stats.views} ❤️${data.stats.loves} ⭐${data.stats.favorites} 🌀${data.stats.remixes} | ${data.author.username}`,
-          author_url:
-            `https://scratch.mit.edu/users/${data.author.username}`,
-          author_icon:
-            data
-              .author
-              .profile
-              .images[
-                '90x90'
-              ],
-          provider_name:
-            'ogratch',
-          provider_url:
-            'https://github.com/nakasyou/ogratch',
-          title:
-            'Scratch',
-          'type':
-            'link',
-          'version':
-            '1.0',
-        }
-      return c
-        .html(
-          html`<!DOCTYPE HTML>${(
-            <html lang='ja'>
-              <head prefix='og: http://ogp.me/ns#'>
-                <meta charset='UTF-8' />
-                <title>
-                  Ogratch
-                </title>
-                <meta
-                  name='viewport'
-                  content='width=device-width, initial-scale=1'
-                />
-                <meta
-                  name='description'
-                  content=''
-                />
-
-                <meta
-                  property='og:site_name'
-                  content='Scratch'
-                />
-                <meta
-                  property='og:title'
-                  content={data
-                    .title}
-                />
-                <meta
-                  property='og:type'
-                  content='article'
-                />
-                <meta
-                  property='og:url'
-                  content={link}
-                />
-                <meta
-                  property='og:image'
-                  content={`https://ogratch.deno.dev/thumbnail/p/${projectId}`}
-                />
-                <meta
-                  property='og:description'
-                  content={desc}
-                />
-                <meta
-                  name='twitter:card'
-                  content='summary_large_image'
-                />
-                <link
-                  rel='alternate'
-                  href={`https://ogratch.deno.dev/echo-json/${
-                    encodeURIComponent(
-                      JSON
-                        .stringify(
-                          embed,
-                        ),
-                    )
-                  }`}
-                  type='application/json+oembed'
-                  title='Scratch'
-                />
-              </head>
-              <body>
-                <a
-                  href={link}
-                >
-                  Push!
-                </a>
-              </body>
-            </html>
-          )}`,
-        )
-    },
+    projRoute,
   )
+app
+  .get(
+    '/projects/:projectId',
+    projRoute,
+  )
+
 app
   .get(
     '/echo-json/:json',
